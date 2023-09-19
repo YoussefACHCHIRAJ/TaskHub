4dc86084ccc2854b5d86e968430ee14f84a23645
YoussefACHCHIRAJ <youssefachchiraj@gmail.com>
2023-09-19 06:47:09 +0100

    modified login page

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -60,17 +60,13 @@ export default function LoginPage() {
           }}
         />
 
-        {mdUp && (
-          <StyledSection>
-            <Typography variant="h3" sx={{ px: 5, mt: 10, mb: 5 }}>
-              Hi, Welcome Back
-            </Typography>
-            <img src="/assets/illustrations/illustration_login.png" alt="login" />
-          </StyledSection>
-        )}
+        
 
         <Container maxWidth="sm">
           <StyledContent>
+          <Typography variant="h3" sx={{  mt: 2, mb: 2 }}>
+              Hi, Welcome Back
+            </Typography>
             <Typography variant="h4" gutterBottom>
               Sign in to TaskHub
             </Typography>
